0e9d8ddcb337f3b233c03a0b311a540d87ff0916
Nguyen Trung Hieu <hieu01.nguyentrung@haravan.com>
2025-01-18 20:58:02 +0700

    update code

--- packages/plugins/pluginCheckScam/src/components/CheckScam/CardLayout.component.tsx
+++ packages/plugins/pluginCheckScam/src/components/CheckScam/CardLayout.component.tsx
@@ -5,6 +5,7 @@ import ModalDialog from '../ModalDialog';
 import GetApi from '../../apis/test';
 import Security from './Security.component';
 import { useEffect } from 'react';
+import Image from 'next/image';
 
 
 const Trending = () => {
@@ -23,7 +24,7 @@ const Trending = () => {
 
   return (
     <div>
-      <p className="mb-5">🔥 Trending</p>
+      <p className="mb-5 fonttext-xl">🔥 Trending</p>
       <div className="grid grid-cols-2 gap-2">
         {api && api?.coins?.map((item:any,index: number) => {
           const itemCoins = item.item;
@@ -31,7 +32,17 @@ const Trending = () => {
           const greenColor = itemCoins?.data.price_change_percentage_24h.usd > 0 ? 'text-green-500' : 'text-red-500';
           return (
             <div key={itemCoins.coin_id} className="flex flex-col items-center justify-between rounded-md bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary cursor-pointer hover:bg-[#4da2ff] bg-[#f7f7f81a]">
-               <p>{itemCoins.symbol}</p>
+              <div className="flex">
+                <Image
+                  width={25}
+                  height={25}
+                  src={itemCoins.large}
+                  className="rounded-full border-[#464C6A]"
+                  alt={itemCoins.name}
+                />
+                <p className="pl-2">{itemCoins.symbol}</p>
+              </div>
+             
               <p>{itemCoins?.data.price.toFixed(2)}$</p>
               <p className={greenColor}>({itemCoins?.data.price_change_percentage_24h.usd.toFixed(2)})%</p>
             </div>
--- packages/plugins/pluginCheckScam/src/components/ModalDialog/index.tsx
+++ packages/plugins/pluginCheckScam/src/components/ModalDialog/index.tsx
@@ -20,7 +20,7 @@ const ModalDialog = ({ trigger, children, className, title = '' }: Props) => {
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger>{trigger}</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="pt-0 bg-[url('https://cdn.prod.website-files.com/6425f546844727ce5fb9e5ab/6568c438caca9358f397a709_Data_cube_v04_v03_1920_coloured-poster-00001.jpg')]">
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
